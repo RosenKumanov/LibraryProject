3982f5548fc67b07e15dbc52127689b83faa65ce
RosenKumanov <94444119+RosenKumanov@users.noreply.github.com>
2022-05-26 14:25:39 +0300

    Registration form now checks for existing user/email Login form added Library and PersonalLibrary added with addBook() and removeBook()

---
+++ library_project/library/PersonalLibrary.java
@@ -0,0 +1,11 @@
+package library_project.library;
+
+import java.util.Set;
+
+public class PersonalLibrary extends Library {
+
+    PersonalLibrary(Set<Book> books) {
+        super(books);
+    }
+
+}
