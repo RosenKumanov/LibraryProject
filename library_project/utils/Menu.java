25392a57a8a00aaa9dd1d88e61696578bf75d6b6
RosenKumanov <94444119+RosenKumanov@users.noreply.github.com>
2022-05-30 18:16:55 +0300

    Added even more pages in Menu() Added editFile() function in IUseFiles interface Added changeEmail() functions in User

--- library_project/utils/Menu.java
+++ library_project/utils/Menu.java
@@ -5,19 +5,24 @@ import library_project.library.Library;
 import library_project.users.User;
 import library_project.users.Users;
 
+import java.io.Console;
 import java.util.Scanner;
 
 public class Menu {
 
-    public static void firstScreen() {
+    public static void start() {
+        firstScreen();
+    }
+
+    private static void firstScreen() {
         System.out.println();
-        System.out.println("-------------------------------------");
+        System.out.println("|-----------------------------------|");
         System.out.println("| " + ConsoleColors.BLUE + "DIGITAL LIBRARY " + ConsoleColors.PURPLE + "\"A-TEAM\"" + ConsoleColors.BLUE + " HOMEPAGE" + ConsoleColors.RESET + " |");
-        System.out.println("-------------------------------------\n\n");
-        System.out.println(ConsoleColors.YELLOW + "1. Register        " + ConsoleColors.GREEN + "2. Log in" + ConsoleColors.RESET);
+        System.out.println("|-----------------------------------|\n\n");
+        System.out.println(ConsoleColors.YELLOW + "    1. Register        " + ConsoleColors.GREEN + "2. Log in\n\n\n\n" + ConsoleColors.RESET);
 
         User currentUser;
-        Library mainLibrary = new Library();
+        Library mainLibrary = Library.generateMainLibrary();
 
         int command = getInput();
         while(command < 1 || command > 2) {
@@ -25,7 +30,7 @@ public class Menu {
             command = getInput();
         }
         if(command == 1) {
-            currentUser = Users.register();
+            Users.register();
             firstScreen();
         }
         else {
@@ -36,9 +41,9 @@ public class Menu {
 
     private static void options(Library library, User user) {
         System.out.println();
-        System.out.println("-=-=-=-=-=-=-");
-        System.out.println("| " + ConsoleColors.BLUE + "MAIN MENU" + ConsoleColors.RESET + " |");
-        System.out.println("-=-=-=-=-=-=-");
+        System.out.println(ConsoleColors.PURPLE + "-=-=-=-=-=-=-");
+        System.out.println("| " + ConsoleColors.BLUE + "MAIN MENU" + ConsoleColors.PURPLE + " |");
+        System.out.println("-=-=-=-=-=-=-" + ConsoleColors.RESET);
         System.out.println("1. Explore books");
         System.out.println("2. My library");
         System.out.println("3. Favourite books");
@@ -58,30 +63,39 @@ public class Menu {
                 exploreBooks(library, user);
                 break;
             case 2:
-
+                myLibrary(library, user);
+                break;
+            case 3:
+                favouriteBooks(library, user);
+                break;
+            case 4:
+                addNewBook(library, user);
+                break;
+            case 5:
+                accountInfo(library, user);
+                break;
+            case 6:
+                firstScreen();
         }
-
-    }
-
-    private static void myLibrary(User user) {
 
     }
 
     private static void exploreBooks(Library library, User user) {
         System.out.println();
-        System.out.println("-=-=-=-=-=-=-=-=-=-");
-        System.out.println("| " + ConsoleColors.BLUE + "EXPLORE BOOKS" + ConsoleColors.RESET + " |");
-        System.out.println("-=-=-=-=-=-=-=-=-=-");
-        System.out.println("Choose a book to interact with: \n");
+        System.out.println(ConsoleColors.PURPLE + "-=-=-=-=-=-=-=-=-=-");
+        System.out.println("| " + ConsoleColors.BLUE + "EXPLORE BOOKS" + ConsoleColors.PURPLE + " |");
+        System.out.println("-=-=-=-=-=-=-=-=-=-" + ConsoleColors.RESET);
 
         int bookCounter = 0;
         if(library.getBooks() == null) {
             System.out.println("No books in our library yet. Sorry!");
-
+            System.out.println("Returning to main menu...\n\n");
+            options(library, user);
         }
         else {
+            System.out.println("Choose a book to interact with: \n");
             for (Book book : library.getBooks()) {
-                System.out.println(++bookCounter + ". " + book.getBookName() + ", written by " + book.getAuthor() + " , Rating: " + book.getBookReview());
+                System.out.println(++bookCounter + ". " + book.getBookName() + ", written by " + book.getAuthor() + " , Rating: " + book.getAverageRating());
             }
 
             int command = getInput();
@@ -90,6 +104,92 @@ public class Menu {
                 System.out.println("You need to type a number between 1-" + bookCounter + ":");
                 command = getInput();
             }
+            //getBookByID(int ID) - a method which returns a book object by provided ID. It should find it in the file
+            //bookInteract(library, user, book);
+        }
+    }
+
+    private static void myLibrary(Library library, User user) {
+        System.out.println();
+        System.out.println(ConsoleColors.PURPLE + "-=-=-=-=-=-=-=-=-=-");
+        System.out.println("| " + ConsoleColors.BLUE + "MY LIBRARY" + ConsoleColors.PURPLE + " |");
+        System.out.println("-=-=-=-=-=-=-=-=-=-" +  ConsoleColors.RESET);
+
+        int bookCounter = 0;
+        if(user.getPersonalLibrary().getBooks() == null) {
+            System.out.println("No books in our library yet. Sorry!");
+            System.out.println("Returning to main menu...\n\n");
+            options(library, user);
+
+        }
+        else {
+            System.out.println("Choose a book to interact with: \n");
+            for (Book book : user.getPersonalLibrary().getBooks()) {
+                System.out.println(++bookCounter + ". " + book.getBookName() + ", written by " + book.getAuthor() + " , Rating: " + book.getAverageRating());
+            }
+
+            int command = getInput();
+
+            while (command < 1 || command > bookCounter) {
+                System.out.println("You need to type a number between 1-" + bookCounter + ":");
+                command = getInput();
+            }
+        }
+    }
+
+    private static void favouriteBooks(Library library, User user) {
+    }
+
+    private static void addNewBook(Library library, User user) {
+        System.out.println("We will soon have an addBook() method :)");
+        options(library, user);
+    }
+
+    private static void accountInfo(Library library, User user) {
+        System.out.println();
+        System.out.println(ConsoleColors.PURPLE + "-=-=-=-=-=-=-=-=-=-");
+        System.out.println("| " + ConsoleColors.BLUE + "ACCOUNT INFO" + ConsoleColors.PURPLE + " |");
+        System.out.println("-=-=-=-=-=-=-=-=-=-\n\n" +  ConsoleColors.RESET);
+        System.out.println("YOUR NAME: " + ConsoleColors.CYAN + user.getFirstName() + ConsoleColors.RESET);
+        System.out.println("USERNAME: " + ConsoleColors.CYAN + user.getUsername() + ConsoleColors.RESET);
+        System.out.println("EMAIL ADDRESS: " + ConsoleColors.CYAN + user.getEmail() + ConsoleColors.RESET);
+        System.out.println();
+        System.out.println("Choose one of the options: ");
+        System.out.println("1. Change password");
+        System.out.println("2. Change email address");
+        System.out.println("3. Go back");
+
+        int input = getInput();
+        while(input < 1 || input > 3) {
+            System.out.println("Type a number between 1-3: ");
+            input = getInput();
+        }
+        if(input == 3) {
+            options(library, user);
+        }
+        else if (input == 2) {
+            user.changeEmail();
+            accountInfo(library, user);
+        }
+    }
+
+    private static void bookInteract(Library library, User user, Book book) {
+        System.out.println("Selected book: ");
+        //print the book name here
+        System.out.println();
+        System.out.println("Options: ");
+        System.out.println("1. Rate");
+        System.out.println("2. Leave a review");
+        System.out.println("3. Edit book");
+        System.out.println("4. Go back");
+
+        int input = getInput();
+        while(input < 1 || input > 4) {
+            System.out.println("Type a number between 1-4: ");
+            input = getInput();
+        }
+        if(input == 4) {
+            options(library, user);
         }
     }
 
